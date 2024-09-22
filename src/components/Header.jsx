b858021381a15584aea98960b27b2ed83bafd5f2
kokis128 <jorgevalentini76@gmail.com>
2024-09-22 19:56:25 -0300

    ajustes en inicio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ export const Header = () => {
     <div>
     <header className='relative grid my-12 col-1 col-span-1  md:col-span-4 '>
 
-<div className='absolute left-[30px]  top-35 '>
-<img src={logo} alt="logo epet24" className='w-11 h-10 md:w-13 md:h-12 pl-3 flex-none hover:h-14 cursor-pointer' /> 
-<h3 className='  text-[8px] md:text-[9px]  font-medium hover:font-bold cursor-pointer pl-4 '>EPET 24</h3>
-<h4 className='hidden md:block text-[7px] md:text-[8px] font-medium hover:font-bold cursor-pointer '>Rincón de los Sauces</h4>
+<div className='absolute left-[30px]  top-35 mb-5 '>
+<img src={logo} alt="logo epet24" className='w-14 h-11 md:w-13 md:h-12 pl-3 flex-none  ' /> 
+<h3 className='  text-[12px] md:text-[12px]  font-medium hover:font-bold  pl-3 whitespace-nowrap  '>EPET 24</h3>
+<h4 className='hidden md:block text-[7px] md:text-[8px] font-medium hover:font-bold  '>Rincón de los Sauces</h4>
 </div>
 
 
