5a5d401a275e98bc446eba77b05bd92a9bb8dbaf
kokis128 <jorgevalentini76@gmail.com>
2024-10-08 00:50:27 -0300

    merge

--- src/pages/estudiantes/CargarEstudiantes.jsx
+++ src/pages/estudiantes/CargarEstudiantes.jsx
@@ -12,8 +12,6 @@ export const CargarEstudiantes = () => {
   const cursoId = id;
   const [form] = Form.useForm();
   const API_URL = process.env.REACT_APP_API_URL;
-<<<<<<< HEAD
-=======
   const  onSelectEstudiante= async (_id)=>{
     try{
       const response =fetch(`${API_URL}/estudiante`, {
@@ -60,7 +58,6 @@ useEffect(() => {
 
   
 console.log(id)
->>>>>>> b619f809ab465e97f98ddebed2ad7c5c656c506a
 
   useEffect(() => {
     fetch(`${API_URL}/estudiantes`)
@@ -118,7 +115,6 @@ console.log(id)
 
   return (
     <>
-<<<<<<< HEAD
       <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gray-200 py-10">
         <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-lg mb-8 md:mb-0 md:mr-8">
           <div className="text-3xl font-bold text-center text-gray-800 mb-6">Cargar Estudiantes</div>
@@ -154,100 +150,6 @@ console.log(id)
                 <th className="py-3 px-4 border-b">Nombre</th>
                 <th className="py-3 px-4 border-b">Apellido</th>
                 <th className="py-3 px-4 border-b">Acciones</th> {/* Nueva columna para acciones */}
-=======
-    <div className="flex flex-col md:flex-row items-center justify-center min-h-screen bg-gray-200 py-10">
-      
-      {/* Formulario */}
-      <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-lg mb-8 md:mb-0 md:mr-8">
-        <div className="flex justify-between mb-6">
-          {cursoDivisionBdRender.filter(curso => curso?._id === id).map((curso, index) => (
-            <div key={index} className="text-lg font-semibold text-gray-700">Curso: {curso?.curso} {curso?.division}</div>
-          ))}
-        </div>
-        <div className="text-3xl font-bold text-center text-gray-800 mb-6">Cargar Estudiantes</div>
-        <Form
-          form={form}
-          layout="vertical"
-          onFinish={onFinish}
-          className="space-y-6"
-        >
-          <Form.Item
-            label="Apellido"
-            name="apellido"
-            rules={[
-              {
-                required: true,
-                message: 'Por favor ingrese el apellido',
-              },
-            ]}
-            className="mb-4"
-          >
-            <Input className="py-2 px-4 border border-gray-300 rounded-md w-full" />
-          </Form.Item>         
-  
-          <Form.Item
-            label="Nombre"
-            name="nombre"
-            rules={[
-              {
-                required: true,
-                message: 'Por favor ingrese el nombre',
-              },
-            ]}
-            className="mb-4"
-          >
-            <Input className="py-2 px-4 border border-gray-300 rounded-md w-full" />
-          </Form.Item>
-  
-          <Form.Item
-            label="DNI"
-            name="dni"
-            rules={[
-              {
-                required: true,
-                message: 'Por favor ingrese el DNI',
-              },
-            ]}
-            className="mb-4"
-          >
-            <Input className="py-2 px-4 border border-gray-300 rounded-md w-full" />
-          </Form.Item>
-  
-          <Form.Item
-            label="Observaciones"
-            name="observaciones"
-            className="mb-4"
-          >
-            <Input className="py-2 px-4 border border-gray-300 rounded-md w-full" />
-          </Form.Item>
-  
-          <Form.Item className="text-center">
-            <Button type="primary" htmlType="submit" className="bg-blue-500 text-white py-2 px-6 rounded-md hover:bg-blue-600 transition duration-200 ease-in-out">
-              Guardar
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
-  
-      {/* Tabla de estudiantes */}
-      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-3xl">
-        <div className="text-2xl font-bold text-center text-gray-800 mb-6">Lista de Estudiantes</div>
-        <table className="table-auto w-full">
-          <thead>
-            <tr className="bg-blue-500 text-white">
-              <th className="py-3 px-4 border-b">DNI</th>
-              <th className="py-3 px-4 border-b">Nombre</th>
-              <th className="py-3 px-4 border-b">Apellido</th>
-            </tr>
-          </thead>
-          <tbody>
-            {estudiantesBd.filter(estudiante => estudiante?.cursoId === cursoId).map((estudiante, index) => (
-              <tr key={index}  className="text-gray-700 bg-white hover:bg-gray-100 transition-colors duration-200 ease-in-out">
-                <td className="py-3 px-4 border-b">{estudiante?.dni}</td>
-                <td className="py-3 px-4 border-b">{estudiante?.nombre}</td>
-                <td className="py-3 px-4 border-b">{estudiante?.apellido}</td>
-                <td><Button onClick={()=>onSelectEstudiante(estudiante?._id)}>Eliminar</Button></td>
->>>>>>> b619f809ab465e97f98ddebed2ad7c5c656c506a
               </tr>
             </thead>
             <tbody>
